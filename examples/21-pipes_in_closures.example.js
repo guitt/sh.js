9beb391b29e6d43cd6526b59999898428d74d3a3
Guillaume Tuton <guillaume@tuton.fr>
2010-12-31 03:15:46 +0100

    Update docs on using `this` in closures

--- examples/21-pipes_in_closures.example.js
+++ examples/21-pipes_in_closures.example.js
@@ -2,7 +2,7 @@
 
 var sh = require('../sh.js');
 
-sh('ls / non_existent_file', function(l) {
-  l.out('grep etc');
-  l.err('sed s/non_existent/NON_EXISTENT/');
+sh('ls / non_existent_file', function() {
+  this.out('grep etc');
+  this.err('sed s/non_existent/NON_EXISTENT/');
 });
--- examples/23-advanced.example.js
+++ examples/23-advanced.example.js
@@ -5,10 +5,10 @@ var sh = require('../sh.js');
 var l = sh('ls / non_existent_file',
   sh.out('grep etc'),
   sh.and('echo ls succeeded'),
-  function(l) {
-    l.err.file('/dev/null');
-    l.or('echo ls failed');
-    l.then('echo ls finished (1)');
+  function() {
+    this.err.file('/dev/null');
+    this.or('echo ls failed');
+    this.then('echo ls finished (1)');
   }
 );
 
